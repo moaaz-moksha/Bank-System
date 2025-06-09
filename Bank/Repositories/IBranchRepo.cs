2e002b479e9f0b425b7ed0a31978b68d9dc6d2ac
moaaz-mohammed <Moaaz.Mohammed-std@sw.iatseg.org>
2025-06-09 21:45:50 +0300

    Update IBranchRepo.cs

--- Bank/Repositories/IBranchRepo.cs
+++ Bank/Repositories/IBranchRepo.cs
@@ -1,4 +1,4 @@
-﻿using Bank.Dtos;
+using Bank.Dtos;
 
 namespace Bank.Repositories
 {
@@ -6,7 +6,7 @@ namespace Bank.Repositories
     {
         public bool PostBranch(BranchDtoPost branchDtoPost);
         public List<BranchDtoGetAll> GettAll();
-        public bool Update(int id, BranchDtoUpdate dto);
+        public void Update(int id, BranchDtoUpdate dto);
         public bool Delete(int id); 
     }
 }
